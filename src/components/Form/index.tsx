39206d770a42913fff10b6d3650c66addf76a6f3
Lucas <lucasamarale@hotmail.com>
2022-12-14 09:16:42 -0300

    Aula 05 - State

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Button from "../Button";
 import style from "./form.module.scss";
+import { v4 as uuidv4 } from "uuid";
 
-class Form extends React.Component {
+class Form extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}> {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adicionarTarefa(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    this.props.setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      { ...this.state, selecionado: false, completado: false, id: uuidv4() },
+    ]);
+    this.setState({
+      tarefa: "",
+      tempo: "00:00",
+    });
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adicionarTarefa.bind(this)}
+      >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
@@ -13,6 +37,10 @@ class Form extends React.Component {
             name="tarefa"
             id="tarefa"
             placeholder="O que você quer estudar?"
+            value={this.state.tarefa}
+            onChange={(ev) =>
+              this.setState({ ...this.state, tarefa: ev.target.value })
+            }
             required
           />
         </div>
@@ -25,11 +53,15 @@ class Form extends React.Component {
             id="tempo"
             min="00:00:00"
             max="01:30:00"
+            value={this.state.tempo}
+            onChange={(ev) =>
+              this.setState({ ...this.state, tempo: ev.target.value })
+            }
             required
           />
         </div>
 
-        <Button>Adicionar</Button>
+        <Button type="submit">Adicionar</Button>
       </form>
     );
   }
